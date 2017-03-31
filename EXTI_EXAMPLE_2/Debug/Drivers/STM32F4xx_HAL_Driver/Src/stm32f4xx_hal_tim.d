6e9309fe4b9cc3e39d65421818617472e383cd2c
joshevers86 <joshevers86@gmail.com>
2017-03-31 16:45:20 +0200

    Examples configure and use of the STM32F401RE-Nucleo board

---
+++ EXTI_EXAMPLE_2/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.d
@@ -0,0 +1,77 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Inc/stm32f4xx_hal_conf.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Inc/mxconstants.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cm4.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Inc/stm32f4xx_hal_conf.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Inc/mxconstants.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/jose/workspaceCortex/EXTI_EXAMPLE_2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
---
+++ GPIO_LED/Debug/Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.d
@@ -0,0 +1,77 @@
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal.o: \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/jose/workspaceCortex/GPIO_LED/Inc/stm32f4xx_hal_conf.h \
+ /home/jose/workspaceCortex/GPIO_LED/Inc/mxconstants.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cm4.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/home/jose/workspaceCortex/GPIO_LED/Inc/stm32f4xx_hal_conf.h:
+
+/home/jose/workspaceCortex/GPIO_LED/Inc/mxconstants.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/jose/STM32Cube/Repository/STM32Cube_FW_F4_V1.13.0/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
